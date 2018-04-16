292f5212d23e7cac9214d798db22d94cf6e6f6db
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-04-16 00:31:33 -0500

    Update FraudDetectionApp.java

--- FraudDetectionApp.java
+++ FraudDetectionApp.java
@@ -1,1 +1,63 @@
+import com.packt.reader.PropertyReader;
+import org.apache.Spark.SparkConf;
+import org.apache.Spark.api.java.function.Function;
+import org.apache.Spark.streaming.api.java.JavaStreamingContext;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.Map;
+import scala.Tuple2;
+import kafka.serializer.StringDecoder;
+import org.apache.Spark.streaming.api.java.*;
+import org.apache.Spark.streaming.kafka.KafkaUtils;
+import org.apache.Spark.streaming.Durations;
 
+public class FraudDetectionApp {
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    private static void main(String[] args) throws Exception {
+        PropertyReader propertyReader = new PropertyReader();
+        CacheIPLookup cacheIPLookup = new CacheIPLookup();
+        SparkConf SparkConf = new SparkConf().setAppName("IP_FRAUD");
+        JavaStreamingContext javaStreamingContext = new JavaStreamingContext(SparkConf, Durations.seconds(3));
+
+        Set<String> topicsSet = new HashSet<>(Arrays.asList(propertyReader.getPropertyValue("topic").split(",")));
+        Map<String, String> kafkaConfiguration = new HashMap<>();
+        kafkaConfiguration.put("metadata.broker.list", propertyReader.getPropertyValue("broker.list"));
+        kafkaConfiguration.put("group.id", propertyReader.getPropertyValue("group.id"));
+
+        JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(
+                javaStreamingContext,
+                String.class,
+                String.class,
+                StringDecoder.class,
+                StringDecoder.class,
+                kafkaConfiguration,
+                topicsSet
+        );
+        JavaDStream<String> ipRecords = messages.map(Tuple2::_2);
+
+        JavaDStream<String> fraudIPs = ipRecords.filter(new Function<String, Boolean>() {
+            @Override
+            public Boolean call(String s) throws Exception {
+                String IP = s.split(",")[0];
+                String[] ranges = IP.split("\\.");
+                String range = null;
+                try {
+                    range = ranges[0];
+                } catch (ArrayIndexOutOfBoundsException ex) {
+
+                }
+                return cacheIPLookup.isFraudIP(range);
+
+            }
+        });
+
+        fraudIPs.dstream().saveAsTextFiles("hdfs://localhost:8020/user/packt/streaming/fraudips", "");
+
+        javaStreamingContext.start();
+        javaStreamingContext.awaitTermination();
+    }
+}
