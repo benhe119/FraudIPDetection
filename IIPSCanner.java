ace64d07f10ebee4d29dcfbfbe62559c508ad58a
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-04-16 00:29:48 -0500

    Update IIPSCanner.java
    /*We have used interface before implementing the class so that we can add more NoSQL databases or any fast lookup service

--- IIPSCanner.java
+++ IIPSCanner.java
@@ -1,1 +1,7 @@
+/*We have used interface before implementing the class so that we can add more NoSQL databases or any fast lookup service*/
 
+public interface IIPScanner {
+
+    boolean isFraudIP(String ipAddresses);
+
+}
